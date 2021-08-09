058dc15f6dac340c5e068f31f078b8026a2d3e50
seanbenhur <seanbenhur@gmail.com>
2021-08-09 11:07:22 +0530

    add sort colors

---
+++ Arrays/sort_colors.py
@@ -0,0 +1,23 @@
+"""Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function."""
+
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        low, mid, high = 0, 0, len(nums)-1
+        while mid<=high:
+            if nums[mid]==0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low = low+1
+                mid = mid+1
+            elif nums[mid]==1:
+                mid = mid+1
+            else:
+                nums[high], nums[mid] = nums[mid], nums[high]
+                high = high-1
--- README.md
+++ README.md
@@ -17,6 +17,7 @@ Geeks for geeks
 [Data Structures and Algorithms in Python- Michael T Goodrich](https://github.com/manishbisht/Competitive-Programming/blob/master/Resources/books/Michael%20T.%20Goodrich%2C%20Roberto%20Tamassia%2C%20Michael%20H.%20Goldwasser-Data%20Structures%20and%20Algorithms%20in%20Python-Wiley%20(2013).pdf)
 
 ### Some Youtube Channels
+[Take u forward](https://www.youtube.com/channel/UCJskGeByzRRSvmOyZOz61ig)
 
 [Errichto](https://www.youtube.com/c/Errichto)
 
@@ -29,8 +30,6 @@ Geeks for geeks
 ## Where I Practice !?
 
 I usually open a room on https://binarysearch.com/ , select the topics I would to practice and practice
-
-Before using binary search I practiced on these sites
 
 Leetcode
 
