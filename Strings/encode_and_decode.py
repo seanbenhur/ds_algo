4255180a73735e58656ced8b220c5850e5a220c1
seanbenhur <seanbenhur@gmail.com>
2022-05-03 07:43:05 +0530

    add more

--- Strings/encode_and_decode.py
+++ Strings/encode_and_decode.py
@@ -7,7 +7,7 @@ class Solution:
         # write your code here
         res = ""
         for s in strs:
-            res += str(len(s)) + "#" + s 
+            res +=str(len(s))+"#"+s
         return res
 
 
@@ -17,12 +17,12 @@ class Solution:
     """
     def decode(self, str):
         # write your code here
-        res,i = [], 0
-        while i < len(str):
-            j = i
-            while str[j] != "#":
+        res, i = [], 0
+        while i< len(str):
+            j = i 
+            while str[j]!="#":
                 j += 1
             length = int(str[i:j])
             res.append(str[j+1:j+1+length])
-            i = j + 1 + length
+            i = j+1+length 
         return res
