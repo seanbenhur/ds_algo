3e4d0b0edd230fde01576c3013c154cd3b91c46c
Sean Benhur <seanbenhur@gmail.com>
2022-05-03 13:00:05 +0530

    Time: 114 ms (35.99%), Space: 14 MB (93.96%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -6,9 +6,13 @@ class Solution:
         
         for right in range(len(s)):
             while s[right] in char_set:
+                #if the char is already in the set remove the left most char
                 char_set.remove(s[left])
+                #move the left pointer
                 left += 1
+            #add the charcter to set
             char_set.add(s[right])
+            #+1 is used since index starts from 0
             res = max(res,(right-left)+1)
         return res
             
