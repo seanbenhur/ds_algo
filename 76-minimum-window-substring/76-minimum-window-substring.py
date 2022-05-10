beed90f41129166e8e66b0b1eae674169ad8984c
Sean Benhur <seanbenhur@gmail.com>
2022-05-10 08:02:49 +0530

    Time: 88 ms (96.19%), Space: 14.6 MB (83.84%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -3,35 +3,34 @@ class Solution:
         if not t or not s:
             return ""
         
-        count_t = {}
-        window = {}
+        count_t, window = {}, {}
         
         for char in t:
-            count_t[char]=1 + count_t.get(char,0)
-            
+            count_t[char] = count_t.get(char,0) + 1
+        
         have, need = 0, len(count_t)
         res, reslen = [-1,-1], float('inf')
         
         l = 0
-        for r in range(len(s)):
-            char = s[r]
+        for r,char in enumerate((s)):
             window[char] = 1 + window.get(char,0)
             
             if char in count_t and window[char]==count_t[char]:
                 have += 1
-            
+                
             while have == need:
-                #update the result
                 if (r-l+1) < reslen:
                     res = [l,r]
-                    reslen = (r-l+1)
+                    reslen = r-l+1
                     
-                #pop from left
+                #pop left
                 window[s[l]] -= 1
                 if s[l] in count_t and window[s[l]] < count_t[s[l]]:
                     have -= 1
                 l += 1
         l,r = res
-        return s[l:r+1] if reslen != float('inf') else ""
+        return s[l:r+1] if reslen!=float('inf') else ""
+        
+        
             
                   
