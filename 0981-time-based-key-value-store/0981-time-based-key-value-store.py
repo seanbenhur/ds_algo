4e63460718370b3854cddac3468036f969074213
Sean Benhur <seanbenhur@gmail.com>
2024-10-22 07:50:32 +0530

    Time: 131 ms (98.05%), Space: 73.6 MB (87.18%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -7,60 +7,29 @@ class TimeMap:
     def set(self, key: str, value: str, timestamp: int) -> None:
         if key not in self.timestamp_dict:
             self.timestamp_dict[key] = []
-
         self.timestamp_dict[key].append((value,timestamp))
-
-
     
     def get(self, key: str, timestamp: int) -> str:
-
         res = ""
         values = self.timestamp_dict.get(key,[])
-        l,r =0, len(values)-1
-        while l <= r:
-            m = (l+r)//2
-            if values[m][1] <= timestamp:
-                res = values[m][0]
-                l = m +1
+
+        if len(values) == 0:
+            return res
+
+
+
+        left, right = 0, len(values) - 1
+        while left <= right:
+            mid = (left+right)//2
+            if values[mid][1] == timestamp:
+                res = values[mid][0]
+                return res
+            elif values[mid][1] < timestamp:
+                res = values[mid][0]
+                left = mid + 1
             else:
-                r = m-1
-
+                right = mid - 1
         return res
-        
-
-    # def get(self, key: str, timestamp: int) -> str:
-
-    #     res = ""
-    #     values = self.timestamp_dict.get(key,[])
-        
-    #     if not values:
-    #         return res 
-
-    #     l = 0
-    #     r = len(values)-1
 
 
-    #     while l<=r:
-    #         m = (l+r)//2
-    #         if values[m][1] == timestamp:
-    #             res = values[m][0]
-
-    #         elif values[m][1] <= timestamp:
-    #             #res = values[m][0]
-    #             l = m +1
-    #         else:
-    #             r = m-1
-
-    #     if values[r][0] > timestamp:
-    #         return ''
-    #     return res
-
-
-
-        
-
-
-# Your TimeMap object will be instantiated and called as such:
-# obj = TimeMap()
-# obj.set(key,value,timestamp)
-# param_2 = obj.get(key,timestamp)
+      
