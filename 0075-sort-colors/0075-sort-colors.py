8bdf5a2fa737af8b0b9a6c5b6927807976097d84
Sean Benhur <seanbenhur@gmail.com>
2024-11-19 21:36:23 +0530

    Time: 0 ms (100%), Space: 16.5 MB (64.65%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -62,46 +62,60 @@ class Solution:
 #         print(f"FINAL ANSWER: {nums}")
 
 
-# class Solution:
+# # class Solution:
+#     def sortColors(self, nums) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#         print("INPUT NUMS",nums)
+#         zeroes,ones,twos = 0,0,0
+#         loop_ones = ones-1
+        
+#         for i in range(len(nums)):
+#             if nums[i] == 0:
+#                 zeroes += 1
+
+#             elif nums[i] == 1:
+#                 ones += 1
+
+#             elif nums[i] == 2:
+#                 twos += 1
+
+#         print(f"ZEROES{zeroes}, ONES{ones} AND TWOS{twos}")
+#         for zero in range(zeroes):            
+#             #print("nums[zero]",nums[zero])
+#             nums[zero] = 0
+        
+        
+#         start_iter = zeroes
+#         for one in range(start_iter,(len(nums)-1)-loop_ones):
+#             nums[one] = 1
+
+
+#         start_iter = zeroes+ones
+#         end_iter = len(nums)
+#         for two in range(start_iter,end_iter):
+#           print("2nd index",two)
+#           print("nums[two]",nums[two])
+#           nums[two] = 2
+
+#         print(f"FINAL ANSWER: {nums}")
+
     def sortColors(self, nums) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        print("INPUT NUMS",nums)
-        zeroes,ones,twos = 0,0,0
-        loop_ones = ones-1
-        
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                zeroes += 1
+        low,mid,high = 0,0,len(nums)-1
 
-            elif nums[i] == 1:
-                ones += 1
+        while mid<=high:
+            if nums[mid] == 0:
+                nums[low],nums[mid] = nums[mid],nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid],nums[high] = nums[high], nums[mid]
+                high -= 1
 
-            elif nums[i] == 2:
-                twos += 1
-
-        print(f"ZEROES{zeroes}, ONES{ones} AND TWOS{twos}")
-        for zero in range(zeroes):            
-            #print("nums[zero]",nums[zero])
-            nums[zero] = 0
-        
-        
-        start_iter = zeroes
-        for one in range(start_iter,(len(nums)-1)-loop_ones):
-            nums[one] = 1
-
-
-        start_iter = zeroes+ones
-        end_iter = len(nums)
-        for two in range(start_iter,end_iter):
-          print("2nd index",two)
-          print("nums[two]",nums[two])
-          nums[two] = 2
-
-        print(f"FINAL ANSWER: {nums}")
-
-
+            
         
                 
         
