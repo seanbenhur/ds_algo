3d1df38a41f83a08e563978011cd5e7b25f55be2
Sean Benhur <seanbenhur@gmail.com>
2025-07-24 07:40:10 +0530

    Time: 0 ms (100%), Space: 21.4 MB (29.69%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,18 +1,7 @@
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counter = Counter(nums)
-        freq = [[] for _ in range(len(nums)+1)]
-        
-
-        # store all the keys and vals
-        for key,val in counter.items():
-            freq[val].append(key)
-
-        res = []
-        print("FREQS BEFORE LOOP",freq)
-        for i in range(len(freq)-1,0,-1):
-            for n in freq[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
-
+    def topKFrequent(self, nums, k: int):
+        count_dict = Counter(nums)
+        keys = count_dict.most_common(k)
+        res = [k for k,v in keys]
+        print(res)
+        return res
